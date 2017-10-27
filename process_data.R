abfc2dce8c4a0aeb1dea65b8f85eb5266b326454
Ott Toomet <otoomet@uw.edu>
2017-10-27 11:56:52 -0700

    propublica stuff inserted here

---
+++ process_data.R
@@ -0,0 +1,15 @@
+### This file contains your main code.
+### Feel free to rename it, or use several files instead.
+### It should contain the code along the following lines:
+
+## 1. create the google civic platform request and httr::GET() the result
+##    you need to include your api key in the request
+## 2. extract the elected officials' data from the result
+## 3. transform the data into a well formatted table
+
+## 4. Get state representatives from propublica API
+##    you need the respective API key.
+## 5. transform it in a form you can use for visualizations
+## 6. pick a representative.
+## 7. get this representative's info
+## 8. get her recent votes.
